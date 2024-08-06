c0a40d24e3826e9d9d015e0a3bbb1d91b31b4d1d
jbennett03 <141188350+jbennett03@users.noreply.github.com>
2024-08-06 15:54:46 -0500

    First upload. trying to find a place to host

---
+++ main.py
@@ -0,0 +1,51 @@
+import streamlit as st 
+import feedparser
+
+# add more feeds
+rss_feeds = {
+  "Select" : "",
+  "DC Comics" : "https://rss.app/feeds/snSl0N41sEhH23cD.xml",
+  "Times of India Top Stories" : "http://timesofindia.indiatimes.com/rssfeedstopstories.cms",
+  "BBC" : "https://www.bbc.com/news/rss.xml",
+  "The Guardian" : "https://www.theguardian.com/international/rss"
+}
+
+
+#Parses an RSS feed and returns a list of articles.
+def parse_rss_feed(url):
+  feed = feedparser.parse(url)
+  articles = []
+  for entry in feed.entries:
+    article = {
+      "title": entry.title,
+      "link": entry.link,
+      "description": entry.summary,
+      "published_at": entry.published
+    }
+    articles.append(article)
+  return articles
+
+
+# Title
+st.title("PyFeeder")
+
+# Choose a Feed
+choose_news_feed = "**Select a News Feed:**"
+rss_feed_selected = st.selectbox(choose_news_feed, rss_feeds.keys())
+st.write(rss_feed_selected)
+selected_rss_feed_url = rss_feeds[rss_feed_selected]
+
+# Collect all Feeds
+all_articles = []
+articles = parse_rss_feed(selected_rss_feed_url)
+all_articles += articles
+
+# Sort articles by datetime
+all_articles.sort(key=lambda article: article["published_at"], reverse=True)
+
+#Display Articles
+for article in all_articles:
+  st.markdown(f"**{article['title']}**")
+  st.markdown(f"{article['description']}", unsafe_allow_html=True)
+  st.markdown(f"Published on: {article['published_at']}")
+  st.markdown(f"Link: [More Info]({article['link']})")
